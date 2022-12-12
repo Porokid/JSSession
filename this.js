05b02e52d283a12921e57774b18d70e64d47d454
Ron <matthewcoching123@gmail.com>
2022-12-12 23:17:29 +0800

    Uncomment

--- this.js
+++ this.js
@@ -1,16 +1,16 @@
 console.log(this);
 
-// let person = {
-//   name: "ron",
-//   age: 21,
-//   hobbies: ["guitar", "computer games", "coding"],
-//   likes: ["books", "movies", "cooking"],
-//   getHobbies: function () {
-//     // console.log(this.hobbies);
-//     this.hobbies.forEach((hobby) => {
-//       console.log(hobby);
-//     });
-//   },
-// };
+let person = {
+  name: "ron",
+  age: 21,
+  hobbies: ["guitar", "computer games", "coding"],
+  likes: ["books", "movies", "cooking"],
+  getHobbies: function () {
+    // console.log(this.hobbies);
+    this.hobbies.forEach((hobby) => {
+      console.log(hobby);
+    });
+  },
+};
 
-// console.log(person.getHobbies());
+console.log(person.getHobbies());
